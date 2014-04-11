550101a804cb48f07042014ac5d413071a0e29d7
Samuel Jacob <samueldotj@gmail.com>
2014-04-10 21:52:53 -0700

    Add very basic testing for Coap request.

--- coap/test/test_request.py
+++ coap/test/test_request.py
@@ -1,9 +1,14 @@
 from ..code_registry import MethodCode, MessageType
 from ..coap import Coap
+import binascii
 
 def test_build_message():
     c = Coap('coap.me')
-    result = c.get('hello')
-    print str(bytearray(result.server_reply_list[0].payload))
+    result1 = c.get('hello')
+    assert str(bytearray(result1.server_reply_list[0].payload)) == '\xffworld'
+
+    result2 = c.get('separate')
+    assert str(bytearray(result2.server_reply_list[0].payload)) == '\xffThat took a long time'
+
     c.destroy()
 
