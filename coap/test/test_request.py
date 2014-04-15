ed2309a1817fc08e77f0f1e0ee32c794c9f9c60c
Samuel Jacob <samueldotj@gmail.com>
2014-04-14 19:48:56 -0700

    Used coap.request() function message functional tests.

--- coap/test/test_request.py
+++ coap/test/test_request.py
@@ -1,14 +1,13 @@
-from ..code_registry import MethodCode, MessageType
-from ..coap import Coap
-import binascii
+from ..coap import request
 
-def test_build_message():
-    c = Coap('coap.me')
-    result = c.get('hello')
+
+def test_simple_message():
+    result = request('coap://coap.me/hello')
     assert result.payload == '\xffworld'
 
-    result = c.get('separate')
+
+def test_separate_response():
+    result = request('coap://coap.me/separate')
     assert result.payload == '\xffThat took a long time'
 
-    c.destroy()
 
